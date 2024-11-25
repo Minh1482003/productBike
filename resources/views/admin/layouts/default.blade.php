3065bf46012732f7190166a754b5b8b7b5873f71
Minh1482003 <147911099+Minh1482003@users.noreply.github.com>
2024-11-25 18:52:39 +0700

    Revert "update project gd2"

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -4,31 +4,41 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"> 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('admin/css/style.css') }}">
+  <!-- Nếu đang ở trang admin thì load thêm CSS và JS của admin -->
+  @vite(['resources/css/admin/style.css', 'resources/js/admin/script.js'])
 
   <title>Document</title>
 </head>
 <body>
   
-  @include('admin.partials.header')
+  <header class="header">
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-3">
+          <div class="inner-logo">
+            <a href="admin/dashhboad">Admin</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 
   <div class="body">
-    @include('admin.partials.sider')
-
-    <div class="main">
-      @yield('content')
+    <div class="sider">
+      <div class="inner-menu">
+        <ul>
+          <li><a href="/admin/dashboard">Tổng quan</a></li>
+          <li><a href="/admin/products">Sản phẩm</a></li>
+          <li><a href="/admin/roles">Nhóm quyền</a></li>
+          <li><a href="/admin/accounts">Danh sách tài khoản</a></li>
+        </ul>
+      </div>
     </div>
+
+    <main>
+      @yield('content')
+    </main>
   </div> 
-
-
-  <!-- Place the first <script> tag in your HTML's <head> -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.8.2/tinymce.min.js"></script>
-
-  <!-- script -->
-  <script src="{{ asset('admin/js/script.js') }}"></script>
-  <script src="{{ asset('admin/js/tinymce.js') }}"></script>
-  <!--End script -->
 
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
--- routes/admin/indexRoute.php
+++ routes/admin/indexRoute.php
@@ -1,17 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// [GET] admin/products
-Route::prefix('products')->group(base_path('routes/admin/ProductRoute.php'));
-
-// [GET] admin/category
-Route::prefix('categorys')->group(base_path('routes/admin/CategoryRoute.php'));
+// [GET] admin/ product
+Route::prefix('products')->group(base_path('routes/admin/productRoute.php'));
 
 // [GET] admin/ dashboard
 // Route::prefix('dashboard')->group(base_path('dashboardRoute.php')); 
-
-// [GET] admin/accounts
-Route::prefix('accounts')->group(base_path('routes/admin/AccountRoute.php'));
-
-// [GET] admin/roles
-Route::prefix('roles')->group(base_path('routes/admin/RoleRoute.php'));
